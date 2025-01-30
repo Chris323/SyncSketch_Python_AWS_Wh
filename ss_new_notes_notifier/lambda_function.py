6fe4ea1ab79874975fd2bb90c22a3a0cf032af03
Chris Ortega <chrisortega94@gmail.com>
2025-01-29 17:49:19 -0800

    new script

--- ss_new_notes_notifier/lambda_function.py
+++ ss_new_notes_notifier/lambda_function.py
@@ -1,1 +1,36 @@
+import json
+import urllib3
+import os
+
+def lambda_handler(event, context):
+    # FOUND IN .ENV
+    whurl = "SLACK_WEBHOOK"
+    message = {"text": ""}
+    send_webhook(whurl, message)
+
+    #AKA reverse api
+def send_webhook(webhook_url, payload):
+    #text = "Hello Hammer Creative, from AWS Lambda"
+    
+    # Create an HTTP pool manager
+    http = urllib3.PoolManager()
+    try:
+        # Send the POST request with JSON payload
+        response = http.request(
+            "POST",
+            webhook_url,
+            body=json.dumps(payload)
+        )
+        
+        # Check for successful response
+        if response.status != 200:
+            print(f"Error: {response.status}")
+        
+        else:
+            print("Webhook sent successfully")
+
+    except Exception as e:
+        # Log the error if the request fails
+        return {"statusCode": 500, "body": f"Error: {str(e)}"}
+
 
