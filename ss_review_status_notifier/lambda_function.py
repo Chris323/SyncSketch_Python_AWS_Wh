890b76ab766f15fce5e949ae9534d1dc6d489fdb
Chris Ortega <chrisortega94@gmail.com>
2025-02-03 12:17:31 -0800

    creat helper function and branch

--- ss_review_status_notifier/lambda_function.py
+++ ss_review_status_notifier/lambda_function.py
@@ -4,7 +4,7 @@ import os
 
 def lambda_handler(event, context):
     # FOUND IN .ENV
-    whurl = "SLACK_WEBHOOK"
+    whurl = match_webhook(event[name])
     message = {"text": f"The approval status of \"{event['item_name'].upper()}\" in \"{event['review']['name'].upper()}\" has changed to \"{event['new_status'].capitalize()}\""}
     send_webhook(whurl, message)
 
@@ -33,3 +33,21 @@ def send_webhook(webhook_url, payload):
         # Log the error if the request fails
         return {"statusCode": 500, "body": f"Error: {str(e)}"}
 
+def match_webhook(input):
+    match input:
+        case "project1":
+            return "SlackWhurl1"
+        case "project2":
+            return "SlackWhurl2"
+        case "project3":
+            return "SlackWhurl3"
+        case "project4":
+            return "SlackWhurl4"
+        case "project1":
+            return "SlackWhurl5"
+        case "project2":
+            return "SlackWhurl6"
+        case "project1":
+            return "SlackWhurl7"
+        case default:
+            return "SlackWhurl"
