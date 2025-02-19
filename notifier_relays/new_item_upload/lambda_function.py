071b856f7df35e3765f27baa894670cd4f23323b
Chris Ortega <chrisortega94@gmail.com>
2025-02-18 18:00:19 -0800

    create message for slack

---
+++ notifier_relays/new_item_upload/lambda_function.py
@@ -0,0 +1,69 @@
+import json
+import urllib3
+import os
+import logging
+
+def lambda_handler(event, context):
+    logging_handler(event)
+    whurl = os.environ['SLACK_WEBHOOK_TESTING'] #for testing channel output
+    #whurl = match_webhook(event["project"]["name"])
+    message = {"text": f"{event}"} #uncomment to output entire event to slack, may bog down speeds
+    send_webhook(whurl, message)
+
+    #AKA reverse api
+def send_webhook(webhook_url, payload):
+    #text = "Hello Hammer Creative, from AWS Lambda"
+    
+    # Create an HTTP pool manager
+    http = urllib3.PoolManager()
+    try:
+        # Send the POST request with JSON payload
+        response = http.request(
+            "POST",
+            webhook_url,
+            body=json.dumps(payload)
+        )
+        
+        # Check for successful response
+        if response.status != 200:
+            print(f"Error: {response.status}")
+        
+        else:
+            print("Webhook sent successfully")
+
+    except Exception as e:
+        # Log the error if the request fails
+        return {"statusCode": 500, "body": f"Error: {str(e)}"}
+
+#Channel webhook selection
+def match_webhook(input):
+    if "mtg -" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_MTG')
+    elif "lor-" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_LOR')
+    elif "som-" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_SOM')
+    elif "umbra" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_UMBRA')
+    elif "forza" in input.lower() or "steel" in input.lower() or "motorsport" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_FORZA')
+    elif "camn-" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_CAMN')
+    elif "jaws" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_JAWS')
+    elif "prm-" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_PRM')
+    elif "kf3-" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_KF3')
+    elif "fn" in input.lower() or "fort" in input.lower():
+        return os.getenv('SLACK_WEBHOOK_FN')
+    #elif "gfsh" in input.lower():
+        #return os.getenv('SLACK_WEBHOOK_GFSH')
+    else:
+        return os.getenv("SLACK_WEBHOOK")
+
+def logging_handler(event):
+    logger = logging.getLogger()
+    logger.setLevel("INFO")
+    logger.info(event)     
+           
